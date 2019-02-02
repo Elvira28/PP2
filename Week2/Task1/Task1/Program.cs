976f54267fe1a8b033702eb13c1fdd3318b41b49
Elvira28 <madikhanepiko@gmail.com>
2019-02-02 10:49:20 +0600

    added task 1

---
+++ Week2/Task1/Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program // recursion method to check whether the string is palindrome or not
+    {
+        public static bool IsPalin( string s, int i ) {
+            if (s[i] != s[s.Length- 1 - i])
+                return false;
+            if (i == s.Length / 2)
+                return true;
+            return IsPalin(s, i + 1);
+        }
+
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            string s = sr.ReadToEnd(); // to read from the "input" file
+
+            if (IsPalin(s, 0)) // call the method to check, whether it is palindrome or not
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+            Console.ReadKey();
+        }
+    }
+}
