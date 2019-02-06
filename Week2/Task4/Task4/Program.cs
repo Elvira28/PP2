41f5ec0fbfcf96b4764c0a8dd9e80fed8f467212
Elvira28 <madikhanepiko@gmail.com>
2019-02-06 15:48:22 +0600

    added Task 4

---
+++ Week2/Task4/Task4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        public static void WithFile() // method to create, cope and delete file
+        {
+            StreamWriter lolkek = new StreamWriter("C:/PP1/keklol.txt"); // creating file
+            lolkek.WriteLine("govnyashka");
+            lolkek.Close();
+            File.Copy("C:/PP1/keklol.txt", "C:/work/keklol.txt"); // copying file
+            File.Delete("C:/PP1/keklol.txt"); // deleting file
+        }
+
+        public static void WithDirectory() // method to create and move file
+        {
+            Directory.CreateDirectory("C:/1 course/zhopa"); // creating directory
+            Directory.Move("C:/1 course/zhopa", "C:/work/zhopa"); // moving directory
+            //Directory.Delete("C:/1 course/zhopa");  // we can not use delete function if we moved file 
+        }
+
+        public static void Main(string[] args) // calling methods 
+        {
+            WithFile();
+            WithDirectory();
+            Console.ReadKey();
+        }
+    }
+}
