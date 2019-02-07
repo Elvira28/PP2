e821844d698dec6e4fb78ffcceff59dd636c5728
Elvira28 <madikhanepiko@gmail.com>
2019-02-07 14:08:07 +0600

    task 4 updated

--- Week2/Task4/Task4/Program.cs
+++ Week2/Task4/Task4/Program.cs
@@ -14,15 +14,16 @@ namespace Task4
             StreamWriter lolkek = new StreamWriter("C:/PP1/keklol.txt"); // creating file
             lolkek.WriteLine("tut dolzhn byt' kakoi-to tekst");
             lolkek.Close();
+            //File.WriteAllText("C:/PP1/keklol.txt", "tut dolzhn byt' kakoi-to tekst"); // another way to create file and fill it in
             File.Copy("C:/PP1/keklol.txt", "C:/work/keklol.txt"); // copying file
             File.Delete("C:/PP1/keklol.txt"); // deleting file
         }
 
         public static void WithDirectory() // method to create and move file
         {
-            Directory.CreateDirectory("C:/1 course/kakashka"); // creating directory
-            Directory.Move("C:/1 course/kakashka", "C:/work/kakashka"); // moving directory
-            //Directory.Delete("C:/1 course/zhopa");  // we can not use delete function if we moved file 
+            Directory.CreateDirectory("C:/1 course/kek"); // creating directory
+            Directory.Move("C:/1 course/kek", "C:/work/kek"); // moving directory
+            //Directory.Delete("C:/1 course/kek");  // we can not use delete function if we moved file 
         }
 
         public static void Main(string[] args) // calling methods 
