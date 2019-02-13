e3ed84dcc6996214f1f63ff8d5fce5762b531264
Elvira28 <madikhanepiko@gmail.com>
2019-02-13 22:06:11 +0600

    demo version, not ready, still some mistakes

--- Week3/Task1/Task1/Program.cs
+++ Week3/Task1/Task1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace Task1
@@ -10,93 +6,178 @@ namespace Task1
     class FarManager
     {
         public int cursor;
-        public string path1;
-        //FileSystemInfo currentFs = null;
+        public string path;
+        public int size;
+        public bool ok;
+        DirectoryInfo direc = null;
+        FileSystemInfo currentFs = null;
+        public bool check;
 
         public FarManager()
         {
-            cursor = 2;
+            cursor = 0;
         }
 
-        public FarManager(string path1)
+        public FarManager(string path)
         {
-            this.path1 = path1;
-            cursor = 2;
+            this.path = path;
+            cursor = 0;
+            direc = new DirectoryInfo(path);
+            size = direc.GetFileSystemInfos().Length;
+            ok = true;
         }
 
-        public void Color(FileSystemInfo fs, int index)
+        public void Color(FileSystemInfo fs, int indx)
         {
-            if (cursor == index)
+            if (cursor == indx)
             {
+                currentFs = fs;
                 Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.White;           
+                Console.ForegroundColor = ConsoleColor.White;
             }
-
-            if (fs.GetType() == typeof(FileInfo))
+            else if (fs.GetType() == typeof(FileInfo))
             {
+                check = false;
                 Console.BackgroundColor = ConsoleColor.Black;
                 Console.ForegroundColor = ConsoleColor.Yellow;
             }
-
             else
             {
+                check = true;
                 Console.BackgroundColor = ConsoleColor.Black;
                 Console.ForegroundColor = ConsoleColor.White;
             }
         }
 
-        public void Screen()
+        public void Korset()
         {
-            DirectoryInfo directory = new DirectoryInfo(path1);
-            FileSystemInfo[] fs = directory.GetFileSystemInfos();
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            FileSystemInfo[] fs = direc.GetFileSystemInfos();
 
-            for (int i = 0; i < fs.Length; i++)
+            for (int i = 0, j = 0; i < fs.Length; i++)
             {
-                if (fs[i].Name[0] == '.')
+                if (ok == false && (fs[i].Name.StartsWith(".") || fs[i].Name.StartsWith("$")))
                 {
                     continue;
                 }
-                Color(fs[i], i);
-                Console.WriteLine(fs[i].Name);
+                Color(fs[i], j);
+                j++;
+                Console.WriteLine(j + ". " + fs[i].Name);
+
             }
         }
 
-        public void ArUp()
-        {
-            cursor++;
-        }
-
-        public void ArDown()
+        public void UA()
         {
             cursor--;
+            if (cursor < 0)
+                cursor = size - 1;
         }
 
-        public void cout ()
+        public void DA()
+        {
+            cursor++;
+            if (cursor == size)
+                cursor = 0;
+        }
+
+        public void CalcS()
+        {
+            direc = new DirectoryInfo(path);
+            FileSystemInfo[] fs = direc.GetFileSystemInfos();
+            size = direc.GetFileSystemInfos().Length;
+
+            for (int i = 0; i < direc.GetFileSystemInfos().Length; i++)
+            {
+                if (fs[i].Name[0] == '.')
+                    size--;
+            }
+
+        }
+
+        public void Rabotay()
         {
             ConsoleKeyInfo cki = Console.ReadKey();
 
-            Screen();
-
-            if (cki.Key == ConsoleKey.UpArrow)
+            while (cki.Key != ConsoleKey.Escape)
             {
-                ArUp();
-            }
+                CalcS();
+                Korset();
+                cki = Console.ReadKey();
+                if (cki.Key == ConsoleKey.UpArrow)
+                    UA();
+                if (cki.Key == ConsoleKey.DownArrow)
+                    DA();
+                if (cki.Key == ConsoleKey.PageDown)
+                {
+                    ok = false;
+                    cursor = 0;
+                }
+                if (cki.Key == ConsoleKey.PageUp)
+                {
+                    ok = true;
+                    cursor = 0;
+                }
 
-            if (cki.Key == ConsoleKey.DownArrow)
-            {
-                ArDown();
+                if (cki.Key == ConsoleKey.Enter)
+                {
+                    cursor = 0;
+                    if (check)
+                    {
+                        path = currentFs.FullName;
+                    }
+
+                    else
+                    {
+                        string str;
+                        path = currentFs.FullName;
+                        StreamReader sr = new StreamReader(path);
+                        str = sr.ReadToEnd();
+                        Console.WriteLine(str);
+                        sr.Close();
+                    }
+                }
+
+                if (cki.Key == ConsoleKey.Backspace)
+                {
+                    cursor = 0;
+                    path = direc.Parent.FullName;
+                }
+
+                if (cki.Key == ConsoleKey.Delete)
+                {
+                    path = currentFs.FullName;
+                    if (check)
+                        Directory.Delete(path);
+                    else
+                        File.Delete(path);
+                }
+
+                if (cki.Key == ConsoleKey.VolumeMute)
+                {
+                    path = currentFs.FullName;
+                    string nn = Path.Combine(path, "\\..");
+                    if (check)
+                    {
+                        File.Move(path, nn + Console.ReadLine());
+                    }
+                    else
+                    {
+                        Directory.Move(path, nn + Console.ReadLine());
+                    }
+                }
             }
         }
     }
-
 
     class Program
     {
         static void Main(string[] args)
         {
-            string path1 = "C:/Users/Acer/Desktop/test";
-            FarManager fm = new FarManager(path1);
-            fm.Screen();
+            string path = "C:/Users/Acer/Desktop/test";
+            FarManager fm = new FarManager(path);
+            fm.Rabotay();
             Console.ReadKey();
         }
     }
