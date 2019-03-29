4437367156e763ff704b0dc7c6d083ede6f924e7
Elvira28 <madikhanepiko@gmail.com>
2019-03-29 23:28:44 +0600

    asd

---
+++ Week7/Car/Car/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Car
+{
+    class Program
+    {
+        public static List<Body> bb = new List<Body>();
+        public static List<Body> wall = new List<Body>();
+        public static bool ok = true;
+        public static ConsoleKeyInfo kf = new ConsoleKeyInfo();
+
+        public static void ReadCar()
+        {
+            StreamReader sr = new StreamReader("car.txt");
+            string[] rows = sr.ReadToEnd().Split('\n');
+            for (int i = 0; i < rows.Length; i++)
+            {
+                for (int j = 0; j < rows[i].Length; j++)
+                    if ((bb != null) && (rows[i][j] == '*'))
+                        bb.Add(new Body(j, i));
+            }
+        }
+
+        public static void ReadWall()
+        {
+            StreamReader sr = new StreamReader("wall.txt");
+            string[] rows = sr.ReadToEnd().Split('\n');
+            for (int i = 0; i < rows.Length; i++)
+                for (int j = 0; j < rows[i].Length; j++)
+                    if (wall != null && rows[i][j] == '*')
+                        wall.Add(new Body(j, i));
+
+            for (int i = wall.Count; i > 0; i--)
+            {
+                Console.Write('*');
+            }
+        }
+
+        public static void Direction(ConsoleKeyInfo kd)
+        {
+            if (kd.Key == ConsoleKey.LeftArrow)
+                ok = false;
+            if (kd.Key == ConsoleKey.RightArrow)
+                ok = true;
+        }
+        public static void Draw()
+        {
+            //kf = Console.ReadKey();
+            //Direction(kf);
+            while (true)
+            {
+                Console.Clear();
+
+                for (int i = bb.Count - 1; i > 0; i--)
+                {
+                    if (ok)
+                    {
+                        bb[i].x++;
+                    }
+                    if (ok == false) bb[i].x--;
+                    Console.SetCursorPosition(bb[i].x, bb[i].y);
+                    Console.Write('*');
+                }
+                Thread.Sleep(300);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            ReadCar();
+            ReadWall();
+
+            Thread thread = new Thread(Draw);
+            thread.Start();
+            while (true)
+            {
+                kf = Console.ReadKey();
+                Direction(kf);
+            }
+    }
+}
---
+++ Week7/ser/ser/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace ser
+{
+    public class Shop
+    {  
+        public string name;
+        public string kupil;
+
+        [XmlIgnore]
+        public static string[] producty = { "pechenka", "fruits", "sandwich", "sok" };
+        public Shop()
+        {
+
+        }
+        public static void Vyvod()
+        {
+            for (int i = 0; i < producty.Length; i++)
+            {
+                Console.WriteLine(producty[i]);
+            }
+        }
+    }
+    class Program
+    {
+
+        public static void SR(Shop shop)
+        {
+            FileStream fs = new FileStream(shop.name, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer sr = new XmlSerializer(typeof(Shop));
+            sr.Serialize(fs, shop);
+            fs.Close();
+        }
+
+        public static void DS(Shop shop)
+        {
+            FileStream fs = new FileStream(shop.name, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer dr = new XmlSerializer(typeof(Shop));
+            shop = dr.Deserialize(fs) as Shop;
+            Console.WriteLine(shop);
+            fs.Close();
+        }
+
+        static void Main(string[] args)
+        {
+
+            Shop shop = new Shop();
+            Console.WriteLine("Hello! What is your name?");
+            shop.name = Console.ReadLine();
+            Console.Clear();
+            Shop.Vyvod();
+            int x = 0, y = 0;
+            Console.SetCursorPosition(x, y);
+            int curs = 0;
+            ConsoleKeyInfo cf = new ConsoleKeyInfo();
+            while (true)
+            {
+                cf = Console.ReadKey();
+
+                if (cf.Key == ConsoleKey.Enter)
+                {
+                    shop.kupil = Shop.producty[curs];
+                    SR(shop);
+                    break;
+                }
+                if (cf.Key == ConsoleKey.DownArrow)
+                {
+                    curs++;
+                    y++;
+                    if (y > 3)
+                    {
+                        y = 0;
+                        curs = 0;
+                    }
+                }
+                if (cf.Key == ConsoleKey.UpArrow)
+                {
+                    curs--;
+                    y--;
+                    if (y < 0)
+                    {
+                        y = 3;
+                        curs = 3;
+                    }
+                }
+                Console.SetCursorPosition(x, y);
+            }
+            Console.Clear();
+            DS(shop);
+        }
+        
+    }
+
+}
