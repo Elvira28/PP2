96d634f6f6d27264648af9d8137122301bad5322
Elvira28 <madikhanepiko@gmail.com>
2019-04-19 20:56:44 +0600

    asd

---
+++ Graphics3/Graphics3/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphics3
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bitmap;
+        Graphics g;
+        bool MouseClicked = false;
+        public int x, y, r = 0;
+        Color[] color = new Color[] { Color.Red, Color.Firebrick, Color.Gold, Color.Purple, Color.WhiteSmoke };
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bitmap);
+            pictureBox1.Image = bitmap;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            r += 5;
+            int index = random.Next(0, color.Length - 1);
+            Pen pen = new Pen(color[index], 3);
+            g.DrawEllipse(pen, x - r, y - r, 2 * r, 2 * r);
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if ( !MouseClicked )
+            {
+                x = e.Location.X;
+                y = e.Location.Y;
+                timer1.Start();
+                MouseClicked = true;
+            }
+            else
+            {
+                timer1.Stop();
+                MouseClicked = false;
+            }
+        }
+    }
+}
